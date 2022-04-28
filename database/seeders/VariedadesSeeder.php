e244aec0d1802a4d98b3a3dd1c7ca11cd5d47185
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-04-27 23:31:26 -0300

    Seeder actualizado a modificaciones

--- database/seeders/VariedadesSeeder.php
+++ database/seeders/VariedadesSeeder.php
@@ -18,91 +18,91 @@ class VariedadesSeeder extends Seeder
             [
                 'id' => 1,
                 'nombre' => 'Topazio',
-                'tipo'=>1, //De Origen
+                'tipo_id'=>1, //De Origen
                 'descripcion' => 'Altitud 1200mts.',
                 'url' => 'https://coffeetigerco.com/producto/topazio-brasil/',
-                'tostaduria' => 1,
+                'tostaduria_id' => 1,
                 'external_img_url' => 'https://coffeetigerco.com/wp-content/uploads/2021/07/TOPAZIO-1.jpg'
             ],
             [
                 'id' => 2,
                 'nombre' => 'Blend Moka',
-                'tipo'=>2, //Blend
+                'tipo_id'=>2, //Blend
                 'descripcion' => 'Altura: 2200 / 1100 mts.',
                 'url' => 'https://coffeetigerco.com/producto/blend-moka-2/',
-                'tostaduria' => 1,
+                'tostaduria_id' => 1,
                 'external_img_url' => 'https://coffeetigerco.com/wp-content/uploads/2021/07/BLENDMOKA.jpg'
             ],
             [
                 'id' => 3,
                 'nombre' => 'Blend Espresso',
-                'tipo'=> 2, //Blend
+                'tipo_id'=> 2, //Blend
                 'descripcion' => 'Recomendado para Espresso',
                 'url' => 'https://coffeetigerco.com/producto/blend-espresso/',
-                'tostaduria' => 1,
+                'tostaduria_id' => 1,
                 'external_img_url' => 'https://coffeetigerco.com/wp-content/uploads/2021/07/BLENDESPRESSO-1.jpg'
             ],
             [
                 'id' => 4,
                 'nombre' => 'Papua Nueva Guinea',
-                'tipo'=>1, //De Origen
+                'tipo_id'=>1, //De Origen
                 'descripcion' => 'Altura: 1650 - 1800 mts.',
                 'url' => 'https://www.penguincoffee.com.ar/productos/papu-nueva-guinea/',
-                'tostaduria' => 3,
+                'tostaduria_id' => 3,
                 'external_img_url' => 'https://d3ugyf2ht6aenh.cloudfront.net/stores/898/689/products/whatsapp-image-2022-03-30-at-9-51-57-am1-83c760aac4ce07c9df16486827096923-640-0.jpeg'
             ],
             [
                 'id' => 5,
                 'nombre' => 'Kenya Post Fermentado',
-                'tipo'=>1, //De Origen
+                'tipo_id'=>1, //De Origen
                 'descripcion' => 'Con un excelente puntaje de 87.5 SCA',
                 'url' => 'https://www.penguincoffee.com.ar/productos/kenya-post-fermentado/',
-                'tostaduria' => 3,
+                'tostaduria_id' => 3,
                 'external_img_url' => 'https://d3ugyf2ht6aenh.cloudfront.net/stores/898/689/products/img-20220307-wa00311-551c3037c5d0949e4216468598465286-640-0.jpg'
             ],
             [
                 'id' => 6,
                 'nombre' => 'Sudestada Blend',
-                'tipo'=> 2, //Blend
+                'tipo_id'=> 2, //Blend
                 'descripcion' => 'Un blend que combina la delicada acidez del Colombia con el denso cuerpo y la cafeína del Robusta.',
                 'url' => 'https://capitanescoffeeco.mitiendanube.com/productos/cafe-sudestada-power-blend-blend-de-la-casa/',
-                'tostaduria' => 4,
+                'tostaduria_id' => 4,
                 'external_img_url' => 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/165/647/products/new_blend_sudestada1-cdf05b9f7ba852cc9115960366155624-640-0.jpg'
             ],
             [
                 'id' => 7,
                 'nombre' => 'México Veracruz Orgánico',
-                'tipo'=>1, //De Origen
+                'tipo_id'=>1, //De Origen
                 'descripcion' => 'Aroma intenso a caramelo, de sabor medio con astringencia pronunciada, con cuerpo y acidez media.',
                 'url' => 'https://capitanescoffeeco.mitiendanube.com/productos/cafe-mexico-veracruz-organico/',
-                'tostaduria' => 4,
+                'tostaduria_id' => 4,
                 'external_img_url' => 'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/165/647/products/new_mexico_veracruz1-2dc1ac833d6bef854515960357156935-640-0.jpg'
             ],
             [
                 'id' => 8,
                 'nombre' => 'Strong Coffee Blend',
-                'tipo'=> 2, //Blend
-                'descripcion' => 'Toda la energía que necesitas para tu entrenamiento. Plus de cafeína en un blend único que combina granos de tipo robusta con arabica. Aroma y retrogusto persistente.',
+                'tipo_id'=> 2, //Blend
+                'descripcion' => 'Toda la energía que necesitas para tu entrenamiento. Plus de cafeína en un blend único que combina granos de tipo_id robusta con arabica. Aroma y retrogusto persistente.',
                 'url' => 'https://capitanescoffeeco.mitiendanube.com/productos/strong-coffee-blend/?variant=340546934',
-                'tostaduria' => 4,
+                'tostaduria_id' => 4,
                 'external_img_url' => 'https://i.imgur.com/2D1Z5a5.jpg'
             ], 
             [
                 'id' => 9,
                 'nombre' => 'Amore Rosso',
-                'tipo'=> 2, //Blend
+                'tipo_id'=> 2, //Blend
                 'descripcion' => 'Inspirado en los sabores de Italia, Amore Rosso es una combinación ideal de granos arábicos y robustas de Sudamérica y Asia, logrando un café intenso y de gran presencia en el paladar, ideal para expresso. Amore Rosso, un blend con sutiles notas dulces y amaderadas, de carácter profundo. Excelente elección para compartir un buen momento.',
                 'url' => 'https://eurocafe.com.ar/tienda-eurocafe/cafe-tostado-natural-amore-rosso-1-kg/',
-                'tostaduria' => 2,
+                'tostaduria_id' => 2,
                 'external_img_url' => 'https://eurocafe.com.ar/wb/wp-content/uploads/2019/07/Amore-Rosso_1KG.jpg'
             ], 
             [
                 'id' => 10,
                 'nombre' => 'Guanes Genuino',
-                'tipo'=>1, //De Origen
+                'tipo_id'=>1, //De Origen
                 'descripcion' => 'Tostado medio/alto',
                 'url' => 'https://eurocafe.com.ar/tienda-eurocafe/cafe-tostado-natural-colombia-x-250-grs/',
-                'tostaduria' => 2,
+                'tostaduria_id' => 2,
                 'external_img_url' => 'https://eurocafe.com.ar/wb/wp-content/uploads/2022/01/GUANESGENUINO.jpg'
             ]       
         ]);
@@ -113,7 +113,7 @@ class VariedadesSeeder extends Seeder
             'nombre' => '',
             'descripcion' => '',
             'url' => '',
-            'tostaduria' => 4,
+            'tostaduria_id' => 4,
             'external_img_url' => ''
         ], 
             */
