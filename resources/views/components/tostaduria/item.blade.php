0c8934a63533943766597a9006a7d9d60310f5ba
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-03 07:29:09 -0300

    Cmabios en item tostaduria

--- resources/views/components/tostaduria/item.blade.php
+++ resources/views/components/tostaduria/item.blade.php
@@ -1,7 +1,6 @@
 <div class="card">
   <div class="card-body">
     <h5 class="card-title">{{ $tostaduria->nombre }}</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
     <a href="/tostadurias/{{$tostaduria->id}}" class="btn btn-primary">Ver sucursales</a>
     <a href="#" class="btn btn-warning disabled">Editar</a>
     <a href="#" class="btn btn-danger disabled">Eliminar</a>
