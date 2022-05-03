86f8f0b8eff3f9b62850d674d354bb54faf33611
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-03 06:48:22 -0300

    Cambios en la UI de las variedades

--- resources/views/components/variedad/card.blade.php
+++ resources/views/components/variedad/card.blade.php
@@ -5,10 +5,10 @@
     <div class="card-body">
         <x-origen.clickable-badges :origenes="$variedad->origenes"/>
         <a class="card-title" href ="/variedades/{{$variedad->id}}">{{ $variedad->nombre }}</a>
-        <p><i>Tostador: <a href="/tostaduria/1">{{$variedad->tostaduria->nombre}}</a></i></p>
+        <p><i>Tostador: <a href="/tostaduria/{{$variedad->tostaduria->id}}">{{$variedad->tostaduria->nombre}}</a></i></p>
         <p class="card-text text-truncate">{{ $variedad->descripcion }}</p>
         <!-- <a href="/variedades/{{$variedad->id}}" class="btn btn-primary">Más Información</a> -->
         <a href="#" class="btn btn-warning disabled">Editar</a>
-        <a href="#" class="btn btn-danger disabled">Eliminar</a>        
+        <a href="#" class="btn btn-danger disabled">Eliminar</a>
     </div>
 </div>
--- resources/views/variedad/index.blade.php
+++ resources/views/variedad/index.blade.php
@@ -11,37 +11,38 @@
             <li class="breadcrumb-item active" aria-current="page">Variedades</li>
           </ol>
         </nav>
-    </div>    
+    </div>
 
     <div class="col buscar ">
       <form action="" method="get">
         @if(request('origen'))
-        <input type="hidden" name="origen" value="{{request('origen')}}">        
+        <input type="hidden" name="origen" value="{{request('origen')}}">
         @endif
 
         @if(request('tostaduria'))
-        <input type="hidden" name="tostaduria" value="{{request('tostaduria')}}">        
+        <input type="hidden" name="tostaduria" value="{{request('tostaduria')}}">
         @endif
         <input type="text" name="search" placeholder="Buscar" class="form-control" value="{{request('search')}}">
-      </form>               
-    </div>    
-  </div> 
+      </form>
+    </div>
+  </div>
 </div>
 
 <div class="container">
   <div class="row row-cols-auto">
+
     <div class="col">
       <x-variedad.filter-origen-dropdown/>
     </div>
 
     <div class="col">
       <x-variedad.filter-tostaduria-dropdown/>
-    </div>        
+    </div>
 
     <div class="col">
       <a class="btn btn-primary" href="/variedades" role="button">Resetear filtros</a>
     </div>
-    
+
   </div>
 </div>
 
