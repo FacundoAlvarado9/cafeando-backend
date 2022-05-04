8a382b426e4c2aa39d8e119b0f0ed348d6bb53ab
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-03 22:37:07 -0300

    Ruta / redirige al menú de variedades

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use App\Http\Controllers\OrigenController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('variedades.index');
 });
 
 Route::get('/dashboard', function () {
