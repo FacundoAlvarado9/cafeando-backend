7246762e1356caa4a71073d5308608de18e9b070
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-14 13:09:28 -0300

    Cambios en componentes blade para formularios

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -2,8 +2,8 @@
 
 <div class="mb-3">
 
-  <label for="{{ $nombre }}" class="form-label">{{ ucwords($nombre) }}</label>
-  <input type="text" name="{{ $nombre }}" placeholder="{{ ucwords($nombre) }}" class="form-control"  {{ $attributes(['value'=>old($nombre)]) }}>
+  <label for="{{ $nombre }}" class="form-label">{{ $attributes['label'] ?? ucwords($nombre) }}</label>
+  <input type="text" name="{{ $nombre }}" placeholder="{{ $attributes['label'] ?? ucwords($nombre) }}" class="form-control"  {{ $attributes(['value'=>old($nombre)]) }}>
 
   @error($nombre)
   <div class="alert alert-danger mt-1" role="alert">
